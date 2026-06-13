632cdaeafd4f0aaacecd1e55d6e8b74b43010063
Gurpreet <ajbagdi6@gmail.com>
2026-06-13 19:07:52 +0530

    a

--- app/blogs/[blogid]/page.tsx
+++ app/blogs/[blogid]/page.tsx
@@ -12,9 +12,12 @@ interface userBlog {
 async function singleBlog({ params }: { params: blogType }) {
   let { blogid } = await params;
   console.log("blogId here", blogid);
-  let singleBlog = await fetch(`"https://blog-app-nextjs-ssqw.vercel.app/api/blog"/${blogid}`, {
-    cache: "no-cache",
-  });
+  let singleBlog = await fetch(
+    `https://blog-app-nextjs-ssqw.vercel.app/api/blog/${blogid}`,
+    {
+      cache: "no-cache",
+    },
+  );
   let response: userBlog = await singleBlog.json();
   console.log("blogId json", response);
   //? resnponsived for all sizes
